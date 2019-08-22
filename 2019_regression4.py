7537074f42ba9efdd92a8b51d6606123e269809c
yasir faiz ahmed <46210222+yasirfaizahmed@users.noreply.github.com>
2019-08-22 21:05:14 +0530

    Update 2019_regression4.py

--- 2019_regression4.py
+++ 2019_regression4.py
@@ -6,15 +6,18 @@ y_data = []    #dependent variable y in data give
 for i in range(0,10,1):    #creating a dataset of 100 values each
     x_data.append(random.randint(1, 20))    #random no. from 1 to 20
     y_data.append(random.randint(1, 20))    #----||-----||-----||---
-    plt.plot(x_data[i], y_data[i],'ro')    #plotting the random data with red dots
 #x_data = [4,8,6,5,9,7,12,3,5]
 #y_data = [4,5,1,6,8,2,10,4,6]
-y_pri = []    #list for predicted var
+y_pri = []    #list for predicted y
 
 J_arr = []
-def best_m():
-    m = 0.01    #slope starts from m = 0.01
-    while(m <= 50):
+perfect_J = 100    #dummy value
+m_initial = 0    #initial value of slope m
+m_increment = 0.01   #resolution of slope m
+m_max = 10    #max value of slope m
+def best_m():    #only for one linear, c= 0  variable, with max_m = 50
+    m = m_initial    #slope starts from m = 0.01
+    while(m <= m_max):
         y_pri.clear()    #clearing the array
         for i in range(len(x_data)):
             y_pri.append(m*x_data[i])    #y = mx plus c , here c is not included
@@ -23,19 +26,32 @@ def best_m():
         for i in range(len(x_data)):    #the cost function is calculated
             J += ((y_pri[i] - y_data[i])**2)/(2*10)
         J_arr.append(J)    #the function value is appended to array
-        m += 0.1
-    print(J_arr)
+        m += m_increment    #incriment in slope
 
 plt.plot(x_data, y_data, 'ro')    #plotting the random data
 plt.show()
 best_m()    #calling the function
+print(perfect_J)
+#print(J_arr)
 
-m_arr_test = []
+m_arr_test = []    #for plotting the J values and taking the best J
 m_test = 0
-while(m_test <= 50):
+i = 0
+while(m_test <= m_max):
     m_arr_test.append(m_test)
-    m_test += 0.01
-plt.plot(m_arr_test, J_arr, 'ro')
+    m_test += m_increment
+    if (J_arr[i] < perfect_J):
+        perfect_J = J_arr[i]
+    i += 1
+
+haha = 0
+for i in range(len(J_arr)):    #to get the best slope m
+    if(J_arr[i] == perfect_J):
+        haha = i
+best_slope = haha*m_increment
+
+print("lowest J: ",perfect_J)
+print("best slope found at: ",haha)
+print("BEST SLOPE: ",best_slope)
+plt.plot(m_arr_test, J_arr)
 plt.show()
-
-
